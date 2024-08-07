2ae62fefe1b22e46e55a2da4a7a11aeafb972a51
G D <themeanmememan@gmail.com>
2024-08-06 21:36:37 -0400

    add all the questions and answers in Javascript

---
+++ script.js
@@ -0,0 +1,93 @@
+//Question 1//
+
+localStorage.setItem("Question1Answer", "Red");
+
+const Question1Answer = localStorage.getItem("Question1Answer");
+
+console.log("Question 1: In The Matrix, what color pill does Neo take to learn the truth about the Matrix?" + Question1Answer);
+
+
+//Question 2//
+
+localStorage.setItem("Question2Answer", "Wakanda");
+
+const Question2Answer = localStorage.getItem("Question2Answer");
+
+console.log("Question 2: Question: What is the name of the fictional African country in Black Panther?" + Question2Answer);
+
+
+//Question 3//
+
+localStorage.setItem("Question3Answer", "Vito Corleone");
+
+const Question3Answer = localStorage.getItem("Question3Answer");
+
+console.log("Question 3: In The Godfather, who is the head of the Corleone family?" + Question3Answer);
+
+
+//Question 4//
+
+localStorage.setItem("Question4Answer", "Velociraptor");
+
+const Question4Answer = localStorage.getItem("Question4Answer");
+
+console.log("Question 4: Question: In Jurassic Park, which dinosaur is famously known for its ability to change color and blend in with its surroundings?" + Question4Answer);
+
+
+//Question 5//
+
+localStorage.setItem("Question5Answer", "Leonardo DiCaprio");
+
+const Question5Answer = localStorage.getItem("Question5Answer");
+
+console.log("Question 5: In Inception, who plays the character Cobb?" + Question5Answer);
+
+
+//Question 6//
+
+localStorage.setItem("Question6Answer", "Johnny Depp");
+
+const Question6Answer = localStorage.getItem("Question6Answer");
+
+console.log("Question 6: Who played the character of Jack Sparrow in Pirates of the Caribbean?" + Question6Answer);
+
+
+//Question 7//
+
+localStorage.setItem("Question7Answer", "Dr. Emmett Brown");
+
+const Question7Answer = localStorage.getItem("Question7Answer");
+
+console.log("Question 7: In Back to the Future, what is the name of the scientist who invents the time machine?" + Question7Answer);
+
+
+//Question 8//
+
+localStorage.setItem("Question8Answer", "Truman Burbank");
+
+const Question8Answer = localStorage.getItem("Question8Answer");
+
+console.log("Question 8: In The Truman Show, what is the name of the character played by Jim Carrey who lives in a reality TV show?" + Question8Answer);
+
+
+//Question 9//
+
+localStorage.setItem("Question9Answer", "Rocket");
+
+const Question9Answer = localStorage.getItem("Question9Answer");
+
+console.log("Question 9: In Guardians of the Galaxy, what is the name of the talking raccoon character voiced by Bradley Cooper?" + Question9Answer);
+
+
+//Question 10//
+
+localStorage.setItem("Question10Answer", "Mufasa");
+
+const Question10Answer = localStorage.getItem("Question10Answer");
+
+console.log("Question 10 In Disney’s The Lion King, what is the name of Simba's father?" + Question10Answer);
+
+
+
+
+
